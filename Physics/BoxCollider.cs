ebd5329b2e270f4cb6dbd4c06b6353da4f6ecef3
asimwebwer <102419401+asimwebwer@users.noreply.github.com>
2026-09-26 11:12:38 +0530

    add collision contact

--- Physics/BoxCollider.cs
+++ Physics/BoxCollider.cs
@@ -5,6 +5,16 @@ namespace Platformer2D.Physics;
 
 public class BoxCollider : Component {
     public Vector2 Size { get; set; }
+
+    public float Left => Entity!.Transform.Position.X;
+    public float Right => Entity!.Transform.Position.X + Size.X;
+    public float Top => Entity!.Transform.Position.Y;
+    public float Bottom => Entity!.Transform.Position.Y + Size.Y;
+    public Vector2 Center => new(
+        (Left + Right) / 2,
+        (Top + Bottom) / 2
+    );
+
     public Rectangle Bounds {
         get {
             var position = Entity!.Transform.Position;
--- Physics/PhysicsSystem.cs
+++ Physics/PhysicsSystem.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using Microsoft.Xna.Framework;
 
 namespace Platformer2D.Physics;
 
 public class PhysicsSystem {
-    
+
     private readonly HashSet<PhysicsBody> _bodies;
     private readonly HashSet<BoxCollider> _colliders;
     private readonly Vector2 _gravity;
@@ -36,13 +37,14 @@ public class PhysicsSystem {
     public void Update(float deltaTime) {
         foreach (var body in _bodies) {
             if (body.Entity == null) continue;
+
             if (body.UseGravity) body.Accelaration = _gravity;
 
             body.Velocity += body.Accelaration * deltaTime;
             body.Entity.Transform.Position += body.Velocity * deltaTime;
-
-            DetectCollisions();
         }
+
+        DetectCollisions();
     }
 
     private void DetectCollisions() {
@@ -50,47 +52,73 @@ public class PhysicsSystem {
 
         for (int i = 0; i < colliders.Length; i++) {
             for (int j = i + 1; j < colliders.Length; j++) {
+
                 var a = colliders[i];
                 var b = colliders[j];
-
-                if (!a.Bounds.Intersects(b.Bounds)) continue;
 
                 var bodyA = a.Entity!.GetComponent<PhysicsBody>();
                 var bodyB = b.Entity!.GetComponent<PhysicsBody>();
 
-                if(bodyA != null && bodyB == null) ResolveCollision(a, b);
-                if(bodyA == null && bodyB != null) ResolveCollision(b, a);
+                if (bodyA is null && bodyB is null) continue;
+
+                BoxCollider moving;
+                BoxCollider other;
+
+                if (bodyA is not null && bodyB is null) {
+                    moving = a;
+                    other = b;
+                }
+                else if (bodyA is null && bodyB is not null) {
+                    moving = b;
+                    other = a;
+                }
+                else {
+                    continue;
+                }
+
+                CollisionContact? contact = DetectCollision(moving, other);
+                if (contact is null) continue;
+                ResolveCollision(contact);
             }
         }
     }
 
-    private void ResolveCollision(BoxCollider moving, BoxCollider platform) {
-       var staticBounds = platform.Bounds;
-       var movingBounds = moving.Bounds;
-       var body = moving.Entity!.GetComponent<PhysicsBody>();
-       if (body == null) return;
+    private CollisionContact? DetectCollision(BoxCollider a, BoxCollider b) {
+        var aBounds = a.Bounds;
+        var bBounds = b.Bounds;
 
-       float overlapX = MathF.Min(movingBounds.Right, staticBounds.Right) - MathF.Max(staticBounds.Left, movingBounds.Left);
-       float overlapY = MathF.Min(movingBounds.Bottom, staticBounds.Bottom) - MathF.Max(movingBounds.Top, staticBounds.Top);
-       
-       var position = moving.Entity!.Transform.Position;
+        if (!aBounds.Intersects(bBounds)) return null;
 
-       if (overlapX < overlapY) {
-            if (movingBounds.Center.X < staticBounds.Center.X) position.X -= overlapX;
-            else position.X += overlapX;
+        float overlapX = MathF.Min(aBounds.Right, bBounds.Right) - MathF.Max(aBounds.Left, bBounds.Left);
+        float overlapY = MathF.Min(aBounds.Bottom, bBounds.Bottom) - MathF.Max(aBounds.Top, bBounds.Top);
 
-            body.Velocity = new Vector2(0, body.Velocity.Y);
+        Vector2 normal;
+        float penetration;
+
+        if (overlapX < overlapY) {
+            normal = aBounds.Center.X < bBounds.Center.X ? new Vector2(-1, 0) : new Vector2(1, 0);
+            penetration = overlapX;
         }
         else {
-            if (movingBounds.Center.Y < staticBounds.Center.Y) {
-                position.Y -= overlapY;
-                body.IsGrounded = true;
-            }
-            else position.Y += overlapY;
-
-            body.Velocity = new Vector2(body.Velocity.X, 0);
+            normal = aBounds.Center.Y < bBounds.Center.Y ? new Vector2(0, -1) : new Vector2(0, 1);
+            penetration = overlapY;
         }
 
-        moving.Entity.Transform.Position = position;
+        return new CollisionContact(a, b, normal, penetration);
+    }
+
+    private void ResolveCollision(CollisionContact contact) {
+        var body = contact.A.Entity?.GetComponent<PhysicsBody>();
+        if (body is null) return;
+
+        var velocity = body.Velocity;
+
+        if (contact.Normal.X != 0) velocity.X = 0;
+        if (contact.Normal.Y != 0) {
+            velocity.Y = 0;
+            if (contact.Normal.Y < 0) body.IsGrounded = true;
+        }
+        contact.A.Entity!.Transform.Position += contact.Penetration * contact.Normal;
+        body.Velocity = velocity;
     }
 }
